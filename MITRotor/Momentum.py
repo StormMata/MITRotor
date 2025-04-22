1b9a153867599156b7514227b5a3c7e2125fb797
StormMata <storm.mata097@gmail.com>
2025-04-22 16:57:34 -0500

    neural net

--- MITRotor/Momentum.py
+++ MITRotor/Momentum.py
@@ -197,7 +197,7 @@ class NeuralNetInduction(MomentumModel):
         self.shear = shear
         self.veer = veer
 
-    def Ct_a(
+    def compute_induction(
         self,
         aero_props: "AerodynamicProperties",
         pitch: float,
@@ -205,14 +205,15 @@ class NeuralNetInduction(MomentumModel):
         yaw: float,
         rotor: "RotorDefinition",
         geom: "BEMGeometry",
+        a: float,
     ) -> ArrayLike:
-        Ct = aero_props.solidity * aero_props.W**2 * aero_props.Cax
+        Ct = aero_props.solidity * aero_props.W**2 * aero_props.C_x
 
         # Predictor(hidden_sizes=[32])     # 2 layers, 32 neurons in the hidden layer
         # Predictor(hidden_sizes=[16, 16]) # 3 layers, 16 neurons in each hidden layer
 
-        NN_a_model = Predictor(hidden_sizes=[32])
-        NN_a_model.load_state_dict(torch.load("/scratch/09909/smata/induction_modeling/FF_NN_modeling/NN_models/E064_L2_N16_Arelu.pth"))
+        NN_a_model = Predictor(hidden_sizes=[64,64])
+        NN_a_model.load_state_dict(torch.load("/scratch/09909/smata/induction_modeling/FF_NN_modeling/NN_models/E128_L3_N64_Arelu_15.pth"))
 
         an = evaluate_model(NN_a_model, geom.mu_mesh, geom.theta_mesh, self.veer, self.shear, Ct)
 
@@ -226,11 +227,12 @@ class NeuralNetInduction(MomentumModel):
         yaw: float,
         rotor: "RotorDefinition",
         geom: "BEMGeometry",
+        a: float,
     ) -> ArrayLike:
-        Ct = aero_props.solidity * aero_props.W**2 * aero_props.Cax
+        Ct = aero_props.solidity * aero_props.W**2 * aero_props.C_x
 
-        NN_a_model = Predictor(hidden_sizes=[32])
-        NN_a_model.load_state_dict(torch.load("/scratch/09909/smata/induction_modeling/FF_NN_modeling/NN_models/E064_L2_N16_Arelu.pth"))
+        NN_a_model = Predictor(hidden_sizes=[64,64])
+        NN_a_model.load_state_dict(torch.load("/scratch/09909/smata/induction_modeling/FF_NN_modeling/NN_models/E128_L3_N64_Arelu.pth"))
 
         an = evaluate_model(NN_a_model, geom.mu_mesh, geom.theta_mesh, self.veer, self.shear, Ct)
 
