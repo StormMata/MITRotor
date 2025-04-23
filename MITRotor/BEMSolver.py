5391ef6b8567b04f7e41d8b3427ba0767451ab34
Storm Mata <99148579+StormMata@users.noreply.github.com>
2025-04-22 20:22:53 -0400

    NN

--- MITRotor/BEMSolver.py
+++ MITRotor/BEMSolver.py
@@ -289,7 +289,7 @@ class BEM:
         NN_a_model = XY_Predictor()
         NN_a_model.load_state_dict(torch.load("/scratch/09909/smata/induction_modeling/FF_NN_modeling/NN_models/E128_L3_N64_Arelu_15.pth"))
 
-        a = evaluate_model(NN_a_model, geom.mu_mesh, geom.theta_mesh, self.shear, self.veer, aero_props.C_x)
+        a = evaluate_model(NN_a_model, self.geometry.mu_mesh, self.geometry.theta_mesh, self.shear, self.veer, self.aerodynamic_model.C_x)
 
         return a, aprime
 
